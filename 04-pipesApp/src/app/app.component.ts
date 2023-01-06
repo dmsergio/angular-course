51a3c4c394d9631626563a088da8fe0a1f77defd
dmsergio <sergiodm.1989@gmail.com>
2023-01-06 11:36:18 +0100

    [ADD] Installing PrimeNG

--- 04-pipesApp/src/app/app.component.ts
+++ 04-pipesApp/src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  name: string = 'PrimeNG and Pipes';
+  obj = {
+    name: 'Sergio',
+    age: 33,
+  }
+
 }
