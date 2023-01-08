d5282130e6215455699418270926541eb5a1f35b
dmsergio <sergiodm.1989@gmail.com>
2023-01-08 19:22:31 +0100

    [IMP] New buttons to interact with the typescript data

--- 04-pipesApp/src/app/sales/pages/no-commons/no-commons.component.ts
+++ 04-pipesApp/src/app/sales/pages/no-commons/no-commons.component.ts
@@ -6,16 +6,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoCommonsComponent implements OnInit {
 
+  // i18nSelect
   name: string = 'Sergio';
   gender: string = 'male';
-
   genderMap = {
     'male': 'invitarlo',
     'female': 'invitarla',
   }
 
+  // i18nPlural
+  clients: string[] = [
+    'Frodo',
+    'Bilbo',
+    'Gandaf',
+    'Sauron',
+    'Legolas',
+    'Elrond',
+  ];
+  clientsAux: string[] = [];
+  clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando',
+  }
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  changePerson() {
+    if (this.gender === 'male') {
+      this.name = 'Inma';
+      this.gender = 'female';
+    } else {
+      this.name = 'Sergio';
+      this.gender = 'male';
+    }
+  }
+
+  removeClient() {
+    if (this.clients.length > 0) {
+      this.clientsAux.push(this.clients.pop()!);
+    }
+  }
+
+  resetClients() {
+    this.clients = [...this.clientsAux];
+    this.clientsAux = [];
+  }
 
 }
