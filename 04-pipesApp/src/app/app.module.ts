0f2df1a39fac64e1181545ddced48190e74639a5
dmsergio <sergiodm.1989@gmail.com>
2023-01-08 16:20:41 +0100

    [IMP] adding extra locales

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -8,8 +8,10 @@ import { SharedModule } from './shared/shared.module';
 
 // change the locale
 import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
 registerLocaleData(localEs);
+registerLocaleData(localFr);
 
 
 
