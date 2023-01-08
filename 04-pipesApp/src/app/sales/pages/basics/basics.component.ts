945f096a20cada4419e5b9bd6fac5e5708d8b770
dmsergio <sergiodm.1989@gmail.com>
2023-01-08 14:54:27 +0100

    [IMP] date pipes

--- 04-pipesApp/src/app/sales/pages/basics/basics.component.ts
+++ 04-pipesApp/src/app/sales/pages/basics/basics.component.ts
@@ -10,6 +10,8 @@ export class BasicsComponent implements OnInit {
   nameUpper: string = 'SERGIO';
   nameFull: string = 'sErGiO dIaZ';
 
+  currentDate: Date = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
